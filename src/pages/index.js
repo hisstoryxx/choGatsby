28dda362de5c0b1ac1f4a2e94d0c6b6b9bea21ea
cho <hisstoryxx@gmail.com>
2023-01-18 15:48:55 +0900

    배포 테스트

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ import Seo from '../components/seo'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>I'm making this by following the Gatsby Tutorial. 배포테스트</p>
       <StaticImage
         alt="ditto"
         src="../images/love.jpeg"
@@ -18,6 +18,7 @@ const IndexPage = () => {
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
         src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
       /> */}
+      
     </Layout>
   )
 }
